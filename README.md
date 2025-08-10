241ef2c5a934936e80f97b4eaa6861b2b86edb56
aoaochan <josejbeck99@yahoo.com>
2025-08-10 15:37:49 +0900

    Nuklear testing

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # MacOpenGLTesting
+
+## Credit
+- glfw: https://github.com/glfw/glfw.git
+- glad: https://github.com/Dav1dde/glad.git
+- Nuklear: https://github.com/Immediate-Mode-UI/Nuklear.git
--- src/main.c
+++ src/main.c
@@ -1,9 +1,21 @@
-#include <glad/glad.h>
-#include <GLFW/glfw3.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include "glad/glad.h"
+#include "GLFW/glfw3.h"
+
+#define NK_INCLUDE_FIXED_TYPES
+#define NK_INCLUDE_STANDARD_IO
+#define NK_INCLUDE_STANDARD_VARARGS
+#define NK_INCLUDE_DEFAULT_ALLOCATOR
+#define NK_INCLUDE_VERTEX_BUFFER_OUTPUT
+#define NK_INCLUDE_FONT_BAKING
+#define NK_INCLUDE_DEFAULT_FONT
+#include "Nuklear/nuklear.h"
+#include "Nuklear/nuklear_glfw_gl3.h"
 
 #define VSYNC 1
+#define MAX_VERTEX_BUFFER 512 * 1024
+#define MAX_ELEMENT_BUFFER 128 * 1024
 
 void glfwInitializeErrorCallback(int error, const char* desc) {
     fprintf(stderr, "GLFW error %d: %s\n", error, desc);
@@ -11,8 +23,13 @@ void glfwInitializeErrorCallback(int error, const char* desc) {
 void framebufferSizeCallback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow* window);
 void printGLInfo(void);
+void ui_do(struct nk_context *ctx);
 
 int main(void) {
+    struct nk_glfw glfw = {0};
+    struct nk_context *ctx;
+    struct nk_font_atlas *atlas;
+
     glfwSetErrorCallback(glfwInitializeErrorCallback);
 
     if (!glfwInit()) {
@@ -49,6 +66,30 @@ int main(void) {
         return EXIT_FAILURE;
     }
 
+    ctx = nk_glfw3_init(&glfw, window, NK_GLFW3_INSTALL_CALLBACKS);
+
+    int win_w, win_h;
+    int fb_w, fb_h;
+    float scale_x, scale_y;
+    glfwGetWindowSize(window, &win_w, &win_h);
+    glfwGetFramebufferSize(window, &fb_w, &fb_h);
+    scale_x = (float)fb_w / (float)win_w;
+    scale_y = (float)fb_h / (float)win_h;
+
+    nk_glfw3_font_stash_begin(&glfw, &atlas);
+
+    struct nk_font_config config = nk_font_config(0);
+    config.oversample_h = 1;
+    config.oversample_v = 1;
+    static const nk_rune ranges[] = {0x0020, 0x007E, 0xAC00, 0xD7A3, 0};
+    config.range = ranges;
+
+    struct nk_font *font = nk_font_atlas_add_from_file(atlas, "./PretendardVariable.ttf", 16 * scale_x, &config);
+    font->handle.height = 16.0f;
+    nk_style_set_font(ctx, &font->handle);
+
+    nk_glfw3_font_stash_end(&glfw);
+
     int fbw, fbh;
     glfwGetFramebufferSize(window, &fbw, &fbh);
     glViewport(0, 0, fbw, fbh);
@@ -58,13 +99,19 @@ int main(void) {
     while (!glfwWindowShouldClose(window)) {
         processInput(window);
 
+        nk_glfw3_new_frame(&glfw);
+        ui_do(ctx);
+
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
+
+        nk_glfw3_render(&glfw, NK_ANTI_ALIASING_ON, MAX_VERTEX_BUFFER, MAX_ELEMENT_BUFFER);
 
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
 
+    nk_glfw3_shutdown(&glfw);
     glfwDestroyWindow(window);
     glfwTerminate();
     return EXIT_SUCCESS;
