3c2e7cfdd693c2ecbc6c62f1d25d01854b407d13
aoaochan <josejbeck99@yahoo.com>
2025-08-10 14:51:03 +0900

    (void)window;

--- src/main.c
+++ src/main.c
@@ -72,6 +72,7 @@ int main(void) {
 
 void framebufferSizeCallback(GLFWwindow* window, int width, int height) {
     glViewport(0, 0, width, height);
+    (void)window;
 }
 
 void processInput(GLFWwindow* window) {
